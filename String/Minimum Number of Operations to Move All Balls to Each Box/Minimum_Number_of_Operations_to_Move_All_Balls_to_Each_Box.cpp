94e47a12fcbbd5a2c728d90c37c16e9c13bb351a
Pratham Madhani <prathamadhani28@gmail.com>
2025-01-07 00:07:26 +0530

    Update Minimum Number of Operations to Move All Balls to Each Box

--- String/Minimum Number of Operations to Move All Balls to Each Box/Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.cpp
+++ String/Minimum Number of Operations to Move All Balls to Each Box/Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.cpp
@@ -17,3 +17,37 @@ public:
         return ans;
     }
 };
+
+// Approach-2: Prefix Sum
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        int n = boxes.size();
+        int sum = 0;
+        int right_ones = 0;
+        for(int i = 0;i<n;i++) {
+            if(boxes[i] == '1') {
+                sum+=i;
+                right_ones++;
+            }
+        }
+
+        vector<int> ans(n,0);
+        int prefix_sum = 0;
+        int left_ones = 0;
+        for(int i = 0;i<n;i++) {
+            int moves = sum + prefix_sum;
+            ans[i] = moves;
+
+            if(boxes[i] == '1') {
+                left_ones++;
+                right_ones--;
+            }
+
+            prefix_sum+=left_ones;
+            sum-=right_ones;
+        }
+
+        return ans;
+    }
+};
--- String/Minimum Number of Operations to Move All Balls to Each Box/Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.java
+++ String/Minimum Number of Operations to Move All Balls to Each Box/Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.java
@@ -16,3 +16,36 @@ class Solution {
         return ans;
     }
 }
+
+// Approach-2: Prefix Sum
+class Solution {
+    public int[] minOperations(String boxes) {
+        int n = boxes.length();
+        int sum = 0;
+        int right_ones = 0;
+        for(int i = 0;i<n;i++) {
+            if(boxes.charAt(i) == '1') {
+                sum+=i;
+                right_ones++;
+            }
+        }
+
+        int[] ans = new int[n];
+        int prefix_sum = 0;
+        int left_ones = 0;
+        for(int i = 0;i<n;i++) {
+            int moves = sum + prefix_sum;
+            ans[i] = moves;
+
+            if(boxes.charAt(i) == '1') {
+                left_ones++;
+                right_ones--;
+            }
+
+            prefix_sum+=left_ones;
+            sum-=right_ones;
+        }
+
+        return ans;
+    }
+}
