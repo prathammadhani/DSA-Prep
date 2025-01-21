ddb73540c847f5273c20a6d7788f543ef420c40d
Pratham Madhani <prathamadhani28@gmail.com>
2025-01-21 23:53:16 +0530

    Add Grid Game

---
+++ Matrix/Grid Game/Grid_Game.cpp
@@ -0,0 +1,16 @@
+// Problem Name: Grid Game
+// Problem Link: https://leetcode.com/problems/grid-game/description/
+
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        long long firstRowSum = accumulate(begin(grid[0]), end(grid[0]), 0LL), secondRowSum = 0;
+        long long ans = LONG_LONG_MAX;
+        for (int i = 0; i < grid[0].size(); i++) {
+            firstRowSum -= grid[0][i];
+            ans = min(ans, max(firstRowSum, secondRowSum));
+            secondRowSum += grid[1][i];
+        }
+        return ans;
+    }
+};
