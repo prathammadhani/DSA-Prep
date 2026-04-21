4ad1e6833d4f3eef61cc5c3a9661d0202192e877
Pratham Madhani <prathamadhani28@gmail.com>
2026-04-21 22:54:06 +0530

    LC-1 Solve Two Sum

---
+++ Array/Two Sum/Two_Sum.java
@@ -0,0 +1,16 @@
+//Problem Name: Two Sum
+//Problem Link: https://leetcode.com/problems/two-sum/description/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        Map<Integer, Integer> mp = new HashMap<>();
+        for(int i = 0;i<n;i++) {
+            if(mp.containsKey(target - nums[i])) {
+                return new int[]{mp.get(target - nums[i]), i};
+            }
+            mp.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
