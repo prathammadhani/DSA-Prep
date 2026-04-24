03d81797d96de45758bbc88b3dd8ebbd4fdf7909
Pratham Madhani <prathamadhani28@gmail.com>
2026-04-24 11:16:24 +0530

    LC-2833 Add another approach to solve furthest point from origin

--- String/Furthest Point From Origin/Furthest_Point_From_Origin.cpp
+++ String/Furthest Point From Origin/Furthest_Point_From_Origin.cpp
@@ -1,6 +1,7 @@
 //Problem Name: Furthest Point From Origin
 //Problem Link: https://leetcode.com/problems/furthest-point-from-origin/
 
+//Approach 1
 class Solution {
 public:
     int furthestDistanceFromOrigin(string moves) {
@@ -19,5 +20,24 @@ public:
             }
         }
         return max(cntl, cntr);
+    }
+};
+
+//Approach 2
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int n = moves.size();
+        int cntl = 0, cntr = 0, cntb = 0;
+        for (int i = 0; i < n; i++) {
+            if (moves[i] == 'L') {
+               cntl++;
+            } else if (moves[i] == 'R') {
+                cntr++;
+            } else {
+                cntb++;
+            }
+        }
+        return abs(cntl - cntr) + cntb;
     }
 };
--- String/Furthest Point From Origin/Furthest_Point_From_Origin.java
+++ String/Furthest Point From Origin/Furthest_Point_From_Origin.java
@@ -1,6 +1,7 @@
 //Problem Name: Furthest Point From Origin
 //Problem Link: https://leetcode.com/problems/furthest-point-from-origin/
 
+//Approach 1
 class Solution {
     public int furthestDistanceFromOrigin(String moves) {
         int n = moves.length();
@@ -18,5 +19,23 @@ class Solution {
             }
         }
         return Math.max(cntl, cntr);
+    }
+}
+
+//Approach 2
+class Solution {
+    public int furthestDistanceFromOrigin(String moves) {
+        int n = moves.length();
+        int cntl = 0, cntr = 0, cntb = 0;
+        for (int i = 0; i < n; i++) {
+            if (moves.charAt(i) == 'L') {
+                cntl++;
+            } else if (moves.charAt(i) == 'R') {
+                cntr++;
+            } else {
+                cntb++;
+            }
+        }
+        return Math.abs(cntl - cntr) + cntb;
     }
 }
