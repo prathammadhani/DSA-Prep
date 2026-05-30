00fcee3fa0db4c8f00f29a15c5212a565360507c
Pratham Madhani <prathamadhani28@gmail.com>
2026-05-30 09:04:40 +0530

    Add selection sort algorithm implementation

---
+++ Sorting/Algorithms/Selection Sort/selection_sort.java
@@ -0,0 +1,20 @@
+//Problem Name - Selection Sort
+//Problem Link - https://www.geeksforgeeks.org/problems/selection-sort/1
+
+class Solution {
+    void selectionSort(int[] arr) {
+        // code here
+        int n = arr.length;
+        for(int i = 0;i<n-1;i++) {
+            int mini = i;
+            for(int j = i+1;j<n;j++) {
+                if(arr[j]<arr[mini]) {
+                    mini = j;
+                }
+            }
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
